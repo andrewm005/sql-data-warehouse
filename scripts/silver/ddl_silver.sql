da6f84c17e83a9254afa95e1cca1a701483faa7e
Andrewm <128175293+andrewm005@users.noreply.github.com>
2026-04-07 17:02:21 -0400

    Add comments to ddl_silver.sql for clarity
    Added comments explaining the purpose of the DDL script and its functionality.

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,3 +1,15 @@
+/*
+============================================================
+DOL Script: Create Silver Tables
+============================================================
+Script Purpose:
+  This script creates tables in the 'silver' schema, dropping existing tables
+  if they already exist.
+Run this script to re-define the DDl structure of the 'silver' table
+============================================================
+*/
+
+
 IF OBJECT_ID ('silver.crm_cust_info', 'U') IS NOT NULL
 	DROP TABLE silver.crm_cust_info;
 
